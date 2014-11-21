2bd10cbd87e511bdc3af15b54d87f5400767b7f3
zhanglin00129 <zhanlgin00129@gmail.com>
2014-11-21 20:00:03 +0800

    output cartList,payList from function

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
             var items = loadAllItems();
 
             _(items).each(function (item) {
-                var addCart = '<button>加入购物车</button>';
+                var addCart = '<button class="addToCart" id="' +item.barcode +'">加入购物车</button>';
                 var listItem = $('<tr>\
                             <td>' + item.name + '</td>\
                             <td>' + item.price + '</td>\
@@ -12,6 +12,10 @@ $(document).ready(function () {
                             <td>' + addCart + '</td>\
                           </tr>');
                 $('#item-table').append(listItem);
+            });
+            var count = $('#count').text();
+            $('.addToCart').click(function(){
+              $('#count').text(++count);
             });
         };
 
@@ -27,5 +31,39 @@ $(document).ready(function () {
 
     feature.init();
     feature.printDate();
+    Cart();
 });
 
+
+  function Cart(){
+    var cartList = [
+      {
+        name:'可口可乐',
+        price:3.00,
+        unit:'瓶',
+        count:1,
+        summary:3.00
+      },
+      {
+        name:'雪碧',
+        price:3.00,
+        unit:'瓶',
+        count:1,
+        summary:3.00
+      },
+    ];
+      var sumPrice = 0;
+      _(cartList).each(function (item) {
+            var itemSummary = item.price*item.count;
+            sumPrice += itemSummary;
+            var CartItem = $('<tr>\
+                  <td>' + item.name + '</td>\
+                  <td>' + item.price.toFixed(2) + '</td>\
+                  <td>' + item.unit + '</td>\
+                  <td>' + item.count + '</td>\
+                  <td>' + itemSummary.toFixed(2) + '</td>\
+                </tr>');
+      $('#cartList').append(CartItem);
+    });
+    $('#sumPrice').prepend(sumPrice.toFixed(2));
+  }
